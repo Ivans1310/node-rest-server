2d6361efe2cc53c95f185cad7362f2bcedcbd25e
Ivan Sanchez <ing.ivaneduardosanchez@hotmail.com>
2019-01-31 08:40:12 -0500

    First commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//=====================================
+// PORT
+//=====================================
+// if the env is on production the port is asigned by the hosting else the port 3000 was by default
+PORT = process.env.PORT || 3000;
+
+module.exports = {
+    PORT
+}
